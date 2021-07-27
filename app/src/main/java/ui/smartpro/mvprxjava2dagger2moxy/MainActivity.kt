6f77c68d64754ed12968c1489042eb3a89bceef1
artemternovenko <artem.inkaz@gmail.com>
2021-07-27 17:02:08 +0600

    Initial commit

---
+++ app/src/main/java/ui/smartpro/mvprxjava2dagger2moxy/MainActivity.kt
@@ -0,0 +1,11 @@
+package ui.smartpro.mvprxjava2dagger2moxy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
