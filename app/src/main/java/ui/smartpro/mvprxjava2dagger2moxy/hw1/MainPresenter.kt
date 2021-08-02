58645cbc1c9decfe18e148f7bfc3f7442c8d5976
artemternovenko <artem.inkaz@gmail.com>
2021-08-02 09:57:12 +0600

    [ADDED] Moxy class MainPresenter

--- app/src/main/java/ui/smartpro/mvprxjava2dagger2moxy/hw1/MainPresenter.kt
+++ app/src/main/java/ui/smartpro/mvprxjava2dagger2moxy/hw1/MainPresenter.kt
@@ -1,24 +1,29 @@
 package ui.smartpro.mvprxjava2dagger2moxy
 
+import moxy.MvpPresenter
 
-class MainPresenter(private val view: MainView, val model: CountersModel){
+// обавляем Moxy
+class MainPresenter(val model: CountersModel) : MvpPresenter<MainView>(){
 //    val model = CountersModel()
 
-    //Архитектурная ошибка. В качестве практического задания - исправить
+
     fun counterClick1() {
         val nextValue = model.next(0)
-        view.setButtonText1(nextValue.toString())
+//        view.setButtonText1(nextValue.toString())
+        viewState.setButtonText1(nextValue.toString())
 
     }
 
     fun counterClick2() {
         val nextValue = model.next(1)
-        view.setButtonText2(nextValue.toString())
+//        view.setButtonText2(nextValue.toString())
+        viewState.setButtonText2(nextValue.toString())
     }
 
 
     fun counterClick3() {
         val nextValue = model.next(2)
-        view.setButtonText3(nextValue.toString())
+//        view.setButtonText3(nextValue.toString())
+        viewState.setButtonText3(nextValue.toString())
     }
 }
