cf199f155b920e11522e297f278795b00d6aad70
artemternovenko <artem.inkaz@gmail.com>
2021-08-20 19:38:20 +0600

    [ADDED] Dagger2 ApplicationComponent.kt

--- app/src/main/java/ui/smartpro/mvprxjava2dagger2moxy/cicerone/App.kt
+++ app/src/main/java/ui/smartpro/mvprxjava2dagger2moxy/cicerone/App.kt
@@ -1,34 +1,26 @@
 package ui.smartpro.mvprxjava2dagger2moxy.cicerone
 
-import android.app.Application
-import android.content.Context
 import com.github.terrakok.cicerone.Cicerone
 import com.github.terrakok.cicerone.Router
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 import io.reactivex.plugins.RxJavaPlugins
 import ui.smartpro.mvprxjava2dagger2moxy.hw5retrofitgithub.data.di.DaggerApplicationComponent
+import ui.smartpro.mvprxjava2dagger2moxy.scheduler.DefaultSchedulers
 
 class App:DaggerApplication() {
 
-    override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
+    override fun applicationInjector(): AndroidInjector<App> =
         DaggerApplicationComponent
             .builder()
             .withContext(applicationContext)
+            .apply {
+                val cicerone = Cicerone.create()
+                withNavigationHolder(cicerone.getNavigatorHolder())
+                withRouter(cicerone.router)
+                withSchedulers(DefaultSchedulers())
+            }
             .build()
-
-    companion object Navigation{
-
-        /**
-         *         Временно до даггера положим это тут
-         */
-
-        private val cicerone: Cicerone<Router> by lazy {
-            Cicerone.create()
-        }
-        val navigatorHolder get() = cicerone.getNavigatorHolder()
-        val router get() = cicerone.router
-   }
 
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/ui/smartpro/mvprxjava2dagger2moxy/hw5retrofitgithub/data/di/ApplicationComponent.kt
+++ app/src/main/java/ui/smartpro/mvprxjava2dagger2moxy/hw5retrofitgithub/data/di/ApplicationComponent.kt
@@ -1,14 +1,19 @@
 package ui.smartpro.mvprxjava2dagger2moxy.hw5retrofitgithub.data.di
 
 import android.content.Context
+import com.github.terrakok.cicerone.NavigatorHolder
+import com.github.terrakok.cicerone.Router
 import dagger.BindsInstance
 import dagger.Component
 import dagger.android.AndroidInjectionModule
+import dagger.android.AndroidInjector
+import ui.smartpro.mvprxjava2dagger2moxy.cicerone.App
+import ui.smartpro.mvprxjava2dagger2moxy.scheduler.Schedulers
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [AndroidInjectionModule::class])
-interface ApplicationComponent {
+interface ApplicationComponent : AndroidInjector<App> {
 
     @Component.Builder
     interface Builder {
@@ -16,6 +21,19 @@ interface ApplicationComponent {
         @BindsInstance
         fun withContext(context: Context): Builder
 
+        @BindsInstance
+        fun withRouter(router: Router): Builder
+
+        @BindsInstance
+        fun withNavigationHolder(navigatorHolder: NavigatorHolder):Builder
+
+        @BindsInstance
+        fun withSchedulers(schedulers: Schedulers): Builder
+
         fun build(): ApplicationComponent
+    }
+
+    override fun inject(instance: App?) {
+        TODO("Not yet implemented")
     }
 }
